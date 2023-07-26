13f011865e3f8a3f5c657a91299392402a4d775a
hong <85050405+anjihong@users.noreply.github.com>
2023-07-27 01:42:56 +0900

    [Silver III] Title: 두 수의 합, Time: 28 ms, Memory: 6776 KB -BaekjoonHub

---
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0), cin.tie(0);
+    int n; cin >> n;
+    map<int,int> m;
+    
+    for (int i=0; i<n; i++) {
+        int a; cin >> a;
+        m.insert({a,i});
+    }
+
+    int x; cin >> x;
+    
+
+    int cnt = 0;
+    for (auto i : m) {
+        int target = x-i.first;
+        if (target != i.first && m.find(target) != m.end() && i.second <= m.find(target)->second) {cnt++;}
+    }
+    cout << cnt;
+}
