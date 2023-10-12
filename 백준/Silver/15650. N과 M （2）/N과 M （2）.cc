a3231a46bbe2d6c83051627e51e5f08ba566588c
hong <85050405+anjihong@users.noreply.github.com>
2023-10-13 00:12:50 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+vector<int> nums;
+bool vis[9];
+int n, m;
+
+void makeCom(int cnt);
+
+int main() {
+    ios::sync_with_stdio(0), cin.tie(0);
+    cin >> n >> m;
+    makeCom(0);
+}
+
+void makeCom(int cnt) {
+    if (cnt == m) {
+        for (auto i : nums) cout << i << " ";
+        cout << '\n';
+        return;
+    }
+
+    for (int i = 1; i <= n; i++) {
+        if (!vis[i] && nums.empty() || i > nums.back()) {
+            vis[i] = true;
+            nums.push_back(i);
+            makeCom(cnt + 1);
+            vis[i] = false;
+            nums.pop_back();
+        }
+    }
+}
