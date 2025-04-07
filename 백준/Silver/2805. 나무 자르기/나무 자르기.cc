26504ab9d6afd240ca8b2a66ae707991bd6301f2
hong <85050405+anjihong@users.noreply.github.com>
2025-04-07 22:35:30 +0900

    [Silver II] Title: 나무 자르기, Time: 152 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.cc
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.cc
@@ -1,39 +1,35 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    
-    long long n, m, max = 0;
-    int tree[1000001];
-    
+long long minL, maxL;
+long long ans;
+int main() {
+    ios::sync_with_stdio(0), cin.tie(0);
+    long long n, m;
     cin >> n >> m;
-    for(int i = 0; i < n; i++) cin >> tree[i];
-    
-    sort(tree, tree + n);
-    
-    long long low = 0;
-    long long high = tree[n - 1];
-    
-    while(low <= high){ //cut 가능할 때 까지
+    vector<int> trees(n);
+    for (auto &tree : trees) {
+        cin >> tree;
+    }
+
+    minL = 0, maxL = *max_element(trees.begin(), trees.end());
+    while (minL <= maxL) {
+        long long mid = (minL + maxL)/2;
         long long sum = 0;
-        long long cut = (low + high) / 2;
-        
-        for(int i = 0; i < n; i++) {
-            if(tree[i] - cut > 0) sum += tree[i] - cut; // cut 하고 남는 게 있다면 가져감
+        for (auto t: trees) {
+            if (t > mid) sum += t - mid;
         }
-        
-        if(sum >= m){ // m미터보다 가져간 나무가 같거나 많으면
-            max = cut; // 현재 cut 지점을 최대 지점으로 저장
-            low = cut + 1; // cut 가능 구간을 더 올림
-        } else{
-            high = cut - 1; // m미터가 안 되면 cut 가능 구간을 내림
+        if (sum >= m) {
+            ans = mid;
+            minL = mid + 1;
+        }
+
+        else {
+            maxL = mid - 1;
         }
     }
-    
-    cout << max; // 최대 cut 지점 출력
-    
-    return 0;
+
+    cout << ans;
+
 }
